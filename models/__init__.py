0fb37e5359392af99c50f0713e53663b7531bf18
PTChinh <9a2.phamtranchinh@gmail.com>
2023-06-07 11:40:19 +0700

    Fix empty task

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,10 @@
 """
 __models__ = ['OrderModel']
 
+from config import Config
 from connect import connect_db, redis_cluster
 from lib import DaoModel
 from models.order import OrderDao
 
 # TemplatesModel = DaoModel(col=connect_db.db.templates, redis=redis_cluster)
+OrderModel = OrderDao(col=connect_db.db.orders, redis=redis_cluster, project=Config.PROJECT, broker=Config.BROKER_URL)

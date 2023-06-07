4d009f968503f6bb657ff3358e91a3323e643569
PTChinh <9a2.phamtranchinh@gmail.com>
2023-06-07 14:04:18 +0700

    Add name services routes

--- resources/__init__.py
+++ resources/__init__.py
@@ -8,10 +8,12 @@ from resources.health_check import HealthCheck
 from resources.hello import HelloWorld
 # from resources.iapi import iapi_resources
 from resources.user import user_nfts_resources
+from resources.ns import ns_resources
 
 api_resources = {
     '/hello': HelloWorld,
     '/common/health_check': HealthCheck,
     # **{f'/iapi{k}': val for k, val in iapi_resources.items()},
     **{f'/user{k}': val for k, val in user_nfts_resources.items()},
+    **{f'/ns{k}': val for k, val in ns_resources.items()},
 }
